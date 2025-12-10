e376b2684d02507850d141900e798a5bde257d40
Jonah Riddle <theforceling@gmail.com>
2025-12-10 18:55:53 -0500

    cities insert (not working)

--- code/sqlstuff.py
+++ code/sqlstuff.py
@@ -1,18 +1,52 @@
 import sqlite3
+from calls import *
 
 
 
 conn = sqlite3.connect("weather_data.db")
 curr = conn.cursor()
 
+curr.execute('DROP TABLE cities')
+
+#Cities
 curr.execute('''
         CREATE TABLE IF NOT EXISTS cities (
-            id INTEGER PRIMARY KEY,
+            city_id INTEGER PRIMARY KEY,
             city TEXT NOT NULL,
+            state_id INTEGER, 
             state TEXT NOT NULL
         )
     ''')
 
+cities = city_list()
+c_id = 0
+s_id = 0
+last = ""
+for c in cities:
+    city = c[0]
+    print(city)
+    state = c[1]
+    print(state)
+    c_id += 1
+    print(f"c = {c_id}")
+    print(f"last = {last}")
+    if state != last:
+        s_id += 1
+        last = state
+        print(f"s = {s_id}")
+        print(f"last = {last}")
+    curr.execute('''
+            INSERT INTO cities 
+                (city_id, city, state_id, state)
+                VALUES (?, ?, ?, ?) 
+                ''', 
+                (c_id, city, s_id, state))
+    
+
+
+    
+        
+#Current Weather
 curr.execute('''
         CREATE TABLE IF NOT EXISTS todays_temp (
             id INTEGER PRIMARY KEY,
@@ -21,6 +55,7 @@ curr.execute('''
         )
     ''')
 
+#Historical Weather
 curr.execute('''
         CREATE TABLE IF NOT EXISTS historic_temp (
             id INTEGER PRIMARY KEY,
