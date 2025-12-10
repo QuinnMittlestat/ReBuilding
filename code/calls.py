4ae4e9dd40aece25977e1d13a407f52db181f9b0
Jonah Riddle <theforceling@gmail.com>
2025-12-10 18:09:04 -0500

    askd

--- code/calls.py
+++ code/calls.py
@@ -4,41 +4,43 @@ from bs4 import BeautifulSoup
 import re
 
 #List of Top Cities
-cities = []
-city_pattern = r"([A-Z]{1}.+)"
+def city_list():
+    cities = []
+    city_pattern = r"([A-Z]{1}.+)"
 
-wiki_url = "https://kids.kiddle.co/List_of_largest_cities_of_U.S._states_and_territories_by_population"
-wiki_r = requests.get(wiki_url)
-soup = BeautifulSoup(wiki_r.content, 'html.parser')
-table = soup.find_all('table', class_="wikitable sortable sort-under")[0]
-#print(type(table))
-#print(table.encode('utf-8'))
-rows = table.find_all('tr')
-#for i in rows:
-#    print(i.encode('utf-8'))
-for row in rows[2:]:
-    boxes = row.find_all('td')
-    names = []
-    for box in boxes:
-        matches = re.findall(city_pattern, box.get_text())
-        for match in matches:
-            try:
-                #print(match)
-                names.append(match)
-            except:
-                #print(match.encode('utf-8'))
-                names.append(match.encode('utf-8'))
-    #print(names)
-    territories = ['American Samoa', 'Guam', 'Northern Mariana Islands', 'Puerto Rico', 'Virgin Islands (U.S.)']
-    if names[0] not in territories:
-        state = names[0]
-        city1 = names[1]
-        if len(names) > 2:
-            city2 = names[2]
-        cities.append((city1, state))
-        if len(names) > 2:
-            cities.append((city2, state))
-print(cities)
+    wiki_url = "https://kids.kiddle.co/List_of_largest_cities_of_U.S._states_and_territories_by_population"
+    wiki_r = requests.get(wiki_url)
+    soup = BeautifulSoup(wiki_r.content, 'html.parser')
+    table = soup.find_all('table', class_="wikitable sortable sort-under")[0]
+    #print(type(table))
+    #print(table.encode('utf-8'))
+    rows = table.find_all('tr')
+    #for i in rows:
+    #    print(i.encode('utf-8'))
+    for row in rows[2:]:
+        boxes = row.find_all('td')
+        names = []
+        for box in boxes:
+            matches = re.findall(city_pattern, box.get_text())
+            for match in matches:
+                try:
+                    #print(match)
+                    names.append(match)
+                except:
+                    #print(match.encode('utf-8'))
+                    names.append(match.encode('utf-8'))
+        #print(names)
+        territories = ['American Samoa', 'Guam', 'Northern Mariana Islands', 'Puerto Rico', 'Virgin Islands (U.S.)']
+        if names[0] not in territories:
+            state = names[0]
+            city1 = names[1]
+            if len(names) > 2:
+                city2 = names[2]
+            cities.append((city1, state))
+            if len(names) > 2:
+                cities.append((city2, state))
+    print(cities)
+    return cities
 
 def geocoding(city,state):
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},US&appid=d49508eda5382fe81a5b8e5b4ce7e539"
